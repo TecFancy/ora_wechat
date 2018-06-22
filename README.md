b3a73f522d7f9a807f991194f636eb0958565ebd
rf.wangchn <rf.wangchn@gmail.com>
2018-06-22 17:08:40 +0800

    init;

---
+++ README.md
@@ -0,0 +1,8 @@
+# wechat
+
+微信公众号和服务号页面
+
+
+wechat/ - 微信公众号页面 - wechat/subscribe/1.0.1/xxx.html
+
+mobile/ - 微信服务号页面 - mobile/wechat/xxx.html
---
+++ mobile/wechat/js/about_us.js
@@ -0,0 +1,8 @@
+$(function(){
+	$(".btn a").click(function(){
+		$('.module-02').css({ 'padding-bottom' : 0 });
+		$(".module-02-bg").css({"height":"30.5rem","overflow":""});
+		$(".btn").hide();
+		$(".module3").css({ "marginTop" : 0 });
+	});
+});
