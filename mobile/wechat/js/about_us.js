c749714b234dd9118779c88f3eca71fda9cca0db
rf.wangchn <rf.wangchn@gmail.com>
2018-07-08 17:40:42 +0800

    更新微信端服务号图片、相关页面结构和样式;

--- mobile/wechat/js/about_us.js
+++ mobile/wechat/js/about_us.js
@@ -1,7 +1,7 @@
 $(function(){
 	$(".btn a").click(function(){
 		$('.module-02').css({ 'padding-bottom' : 0 });
-		$(".module-02-bg").css({"height":"30.5rem","overflow":""});
+		$(".module-02-bg").css({"height":"12rem","overflow":""});
 		$(".btn").hide();
 		$(".module3").css({ "marginTop" : 0 });
 	});
